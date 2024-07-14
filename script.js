83e53568cc6155baa469fbfe6e8ebf5034c73dd4
Aiden <79894783+C0deNameAiden@users.noreply.github.com>
2024-07-14 14:17:06 -0400

    fin

--- script.js
+++ script.js
@@ -4,3 +4,8 @@
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.querySelectorAll('.project-btn').forEach(button => {
+  const href = button.getAttribute('onclick').match(/window\.open\('(.*?)'/)[1];
+  button.setAttribute('onclick', `window.open('${href}', '_blank')`);
+});
